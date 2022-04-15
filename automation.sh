af7643b08034b1c1f877416a3caff47ed38301e6
Jose G. Juarez R <josejuarez@outlook.com>
2022-04-15 14:15:19 -0500

    Creating automation.sh file

--- automation.sh
+++ automation.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
-
-echo "hello world"
+echo "*** Building directory structure ***"
+mkdir -p tempdir/{templates,static}
+cp -a ./static/. ./tempdir/static
+cp -a ./templates/. ./tempdir/templates
+cp ./desafio2_app.py ./tempdir/
+cd tempdir
+echo "*** Creating Dockerfile ***"
+touch Dockerfile
+echo "FROM python" >> DOCKERFILE
+echo "RUN pip install flask" >> DOCKERFILE
+echo "COPY ./static /home/myapp/static/" >> DOCKERFILE
+echo "COPY ./templates /home/myapp/templates/" >> DOCKERFILE
+echo "COPY desafio2_app.py /home/myapp/" >> DOCKERFILE
+echo "EXPOSE 5050" >> DOCKERFILE
+echo "CMD python3 /home/myapp/desafio2_app.py" >> DOCKERFILE
+sudo chmod 666 /var/run/docker.sock
+echo "*** Building Docker Image ***"
+docker build -t python-app .
+echo "*** Running Docker Container ***"
+docker run -t -d -p 5050:5050 --name python-app-running python-app
+echo "*** Listing Docker Container ***"
+docker ps -a
+echo "*************** PROCESS COMPLETED! Please visit  http://localhost:5050 "
